ab856fa824a00684082141433ec2de74bc7cf5d9
Linus Miller <lohfu@lohfu.io>
2015-06-08 09:56:05 +0200

    Stuffs.

--- models/organization.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(mongoose) {
-	var OrganizationSchema = new mongoose.Schema({
-		dateCreated: { type: Date, default: Date.now }
-	});
-
-	mongoose.model('Organization', OrganizationSchema);
-};
