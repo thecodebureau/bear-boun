0b62cc2b6631b0bbbd96532c24b89a6f9f191b72
Linus Miller <lohfu@lohfu.io>
2015-07-31 16:11:44 +0200

    Default routes file.

--- files/server/routes/_index.js
+++ files/server/routes/_index.js
@@ -1,19 +1,10 @@
-var _ = require('lodash');
-
-module.exports = function(mw, config, epiphany) {
-	var navigation = [];
-
+module.exports = function(mw, epiphany) {
 	return _.map(epiphany.navigation.public, function(route) {
 		var mw = _.compact([ function(req, res, next) {
 			res.page = route;
-			res.navigation = _.map(navigation, function(item) {
-				return item === route ? _.assign({ current: true }, item) : item;
-			});
 
 			next();
 		} ].concat(route.mw));
-
-		if (route.nav) navigation.push(route);
 
 		return [ 'get', route.path || '/' + route.name, mw ];
 	});
