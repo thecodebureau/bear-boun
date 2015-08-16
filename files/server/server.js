991e88b22ef49874a447b7d8d18091fd259fa29f
Linus Miller <linus.miller@thecodebureau.com>
2015-08-16 12:22:37 +0200

    Updated default files that are copied.

--- files/server/server.js
+++ files/server/server.js
@@ -1,3 +1,9 @@
 var Epiphany = require('epiphany');
 
-var server = new Epiphany();
+var server = new Epiphany({
+	modules: require('./modules'),
+	pages: require('./pages'),
+	start: false
+});
+
+server.start();
